31b29629147bb92b2e2acab99bb42482240450ac
cbonar <cbonar@localhost>
2008-03-24 12:46:26 +0000

    + minilib : a light replacement for prototype

---
+++ ciform/trunk/src/minilib.js
@@ -0,0 +1,118 @@
+/**
+	@fileoverview
+
+	This library provides a very minimalistic set of functions to fill some gaps in the core Javascript language.<br>
+	It should be compatible with most current market's libraries, so that if size would not be a problem it could easily be replaced/enhanced with one of them.<br>
+
+	@author cbonar at users dot sf dot net
+*/
+
+
+
+function $defined( name )
+{
+	return name != null && name != undefined;
+}
+
+
+
+/**
+	Light implementation of the $ function of modern libraries : tries to find a DOM node from a versatile key.
+
+	@param fieldName the name of the field to find, or the field itself
+	@return The DOM node corresponding to the parameters or undefined if not found
+*/
+function $( fieldName )
+{
+	return typeof fieldName == "object" ? fieldName : document.getElementById(fieldName);
+}
+
+
+
+/**
+	Copies all fields from the given arguments into this object.
+	@addon
+*/
+Object.prototype.extend = function()
+{
+	for ( var a=0 ; a<arguments.length ; a++ )
+	{
+		if ( typeof arguments[a] == "object" )
+		{
+			for ( var f in arguments[a] )
+			{
+				this[f] = arguments[a][f];
+			}
+		}
+	}
+	return this;
+}
+
+
+
+/**
+	Creates a new object with the properties of several objects merged together.<br>
+	The properties of the latest argument overrides the ones of the previous arguments.
+*/
+function merge()
+{
+	var merged = new Object();
+	Object.extend.apply(merged,arguments);
+	return merged;
+}
+
+
+
+/**
+	Compares two objects of any type.
+
+	@param noCase	If both objects are Strings, do a case insensitive comparison
+	@return true if the objects are equals, else false
+*/
+function equals( o1, o2, noCase )
+{
+	return (noCase && typeof o1 == "string" && typeof o2 == "string") ? (o1.toUpperCase() == o2.toUpperCase()) : (o1 == o2);
+}
+
+
+
+/**
+	@see #equals
+*/
+function equalsNoCase( o1, o2 )
+{
+	return equals(o1,o2,true);
+}
+
+
+
+/**
+	@param noCase If the value is a String, use a case insensitive test to search this object
+	@return true if this array contains the given value (not key)
+	@addon
+*/
+Array.prototype.contains = function( value, noCase )
+{
+	var notCaseSensitive = noCase ? true : false;
+
+	for ( e in this ) {
+		if ( equals(this[e],value,notCaseSensitive) ) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+
+/**
+	@see Array#contains
+	@see #equalsNoCase
+	@addon
+*/
+Array.prototype.containsNoCase = function( value )
+{
+	return this.contains(value,true);
+}
+
